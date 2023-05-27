e83ad60e772b5b1ccb523d063914fae58816d64d
MCH <70763505+mohamed25ch@users.noreply.github.com>
2023-05-27 10:00:30 -0400

    Receive Action power On/Off via onReceive()

--- app/src/main/java/com/example/powerreceiversample/CustomReceiver.kt
+++ app/src/main/java/com/example/powerreceiversample/CustomReceiver.kt
@@ -3,11 +3,27 @@ package com.example.powerreceiversample
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.IBinder
+import android.widget.Toast
 
 class CustomReceiver : BroadcastReceiver() {
 
+
+init {
+    var hello = "Hello"
+}
     override fun onReceive(context: Context, intent: Intent) {
-        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
-        TODO("CustomReceiver.onReceive() is not implemented")
+        val intentAction = intent.action
+
+        intentAction?.let {
+            var toastMessage = "unknown intent action"
+            when(it){
+                Intent.ACTION_POWER_CONNECTED -> toastMessage = "Power connected"
+                Intent.ACTION_POWER_DISCONNECTED -> toastMessage = "Power disconnected!"
+            }
+
+            Toast.makeText(context, toastMessage, Toast.LENGTH_SHORT).show()
+
+        }
     }
 }
--- app/src/main/java/com/example/powerreceiversample/MainActivity.kt
+++ app/src/main/java/com/example/powerreceiversample/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.powerreceiversample
 
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.logging.Filter
 
 class MainActivity : AppCompatActivity() {
+
+
+    private val customReceiver = CustomReceiver()
+    private lateinit var filter: IntentFilter
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        filter = IntentFilter()
+
+        filter.addAction(Intent.ACTION_POWER_DISCONNECTED)
+        filter.addAction(Intent.ACTION_POWER_CONNECTED)
+
+        // Register the receiver using the activity context.
+        this.registerReceiver(customReceiver,filter)
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onDestroy() {
+        this.unregisterReceiver(customReceiver)
+        super.onDestroy()
     }
 }
